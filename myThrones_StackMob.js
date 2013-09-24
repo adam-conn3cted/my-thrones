137ce5f8350447111958ce70f786aa1f2c06a79d
Adam Hynes <adamhynes@apalomo-wn.linkedin.biz>
2013-09-24 12:42:09 +1000

    Testing

--- myThrones_StackMob.js
+++ myThrones_StackMob.js
@@ -1,28 +1,37 @@
 function createQuestion()
 {
-  var text = $("#createQuestionForm input[name=text]").val();
-  var correct_answer = $("#createQuestionForm input[name=correct_answer]").val();
-  var wrong_answer_1 = $("#createQuestionForm input[name=wrong_answer_1]").val();
-  var wrong_answer_2 = $("#createQuestionForm input[name=wrong_answer_2]").val();
-  var wrong_answer_3 = $("#createQuestionForm input[name=wrong_answer_3]").val();
+  StackMob.customcode('hello_world', {}, 'GET', {
+  success: function(result) {
+    alert('Success:' + result)
+  },
+  error: function(result) {
+    alert('fail:' + result)
+  }
+})
 
-  var question = new Question({
-    text : text,
-    correct_answer : correct_answer,
-    wrong_answers : [wrong_answer_1, wrong_answer_2, wrong_answer_3],
-    correctly_answered : false,
-    has_been_asked : false
-  });
+  // var text = $("#createQuestionForm input[name=text]").val();
+  // var correct_answer = $("#createQuestionForm input[name=correct_answer]").val();
+  // var wrong_answer_1 = $("#createQuestionForm input[name=wrong_answer_1]").val();
+  // var wrong_answer_2 = $("#createQuestionForm input[name=wrong_answer_2]").val();
+  // var wrong_answer_3 = $("#createQuestionForm input[name=wrong_answer_3]").val();
 
-  question.create({
-    async : false,
-    success : function(model) {
-      alert("Created new question successfully");
-      console.log(model.toJSON());
-    },
-    error : function(model, response) {
-      alert("Could not create question");
-      console.log(response);
-    }
-  });
+  // var question = new Question({
+  //   text : text,
+  //   correct_answer : correct_answer,
+  //   wrong_answers : [wrong_answer_1, wrong_answer_2, wrong_answer_3],
+  //   correctly_answered : false,
+  //   has_been_asked : false
+  // });
+
+  // question.create({
+  //   async : false,
+  //   success : function(model) {
+  //     alert("Created new question successfully");
+  //     console.log(model.toJSON());
+  //   },
+  //   error : function(model, response) {
+  //     alert("Could not create question");
+  //     console.log(response);
+  //   }
+  // });
 }
